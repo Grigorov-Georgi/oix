b49762182875314fe345e8a209a82ae37a7bbe9a
Georgi Grigorov <72125420+Grigorov-Georgi@users.noreply.github.com>
2022-07-12 13:45:17 +0300

    Adding spring init

---
+++ src/main/java/bg/softuni/oix/OixApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.oix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OixApplication.class, args);
+	}
+
+}
