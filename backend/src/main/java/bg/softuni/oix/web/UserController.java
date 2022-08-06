5845022334c1bdee59651612e36acf793a2d47ed
Georgi Grigorov <grigorov_georgi@yahoo.com>
2022-08-06 23:08:45 +0300

    Added changes to html files

--- backend/src/main/java/bg/softuni/oix/web/UserController.java
+++ backend/src/main/java/bg/softuni/oix/web/UserController.java
@@ -1,16 +1,29 @@
 package bg.softuni.oix.web;
 
+import bg.softuni.oix.model.entity.UserEntity;
+import bg.softuni.oix.model.user.OixUserDetails;
+import bg.softuni.oix.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public String login(){
         return "auth-login";
@@ -23,5 +36,12 @@ public class UserController {
         redirectAttributes.addFlashAttribute("bad_credentials", true);
 
         return "redirect:/users/login";
+    }
+
+    @GetMapping("/profile")
+    public String myProfile(Model model, @AuthenticationPrincipal OixUserDetails oixUserDetails){
+        UserEntity user = userService.findById(oixUserDetails.getId());
+        model.addAttribute("user", user);
+        return "my-profile";
     }
 }
